1c937837d9a033a11610e6ca0acb4ebcb0021ea9
Richard West <rwest@mit.edu>
2010-06-04 12:10:42 -0400

    Updated dasslTest to solve for a vector y

--- dasslTest.py
+++ dasslTest.py
@@ -8,15 +8,15 @@ class MyModel(dassl.DASSL):
 	
 	def residual(self, t, y, dydt):
 		delta = np.zeros(y.shape[0], np.float64)
-		delta[0] = -y[0] - dydt[0]
+		delta = -y - dydt
 		return delta, 0
 	
-	#def jacobian(self, t, y, dydt, cj):
-	#	pd = np.zeros((y.shape[0],y.shape[0]), np.float64)
-	#	pd[0,0] = -1 - cj
-	#	return pd
+	def jacobian(self, t, y, dydt, cj):
+		pd = np.ones(y.shape[0], np.float64)
+		pd = pd * (-1 - cj)
+		return np.diag(pd)
 	
 d = MyModel()
-d.initialize(0, [1.0])
+d.initialize(0, [1.0, 2.0, 10.0])
 d.advance(1.0)
 print d.t, d.y
