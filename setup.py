eb75afaaae644b4c40b8c9014246203b3ed90583
Richard West <rwest@mit.edu>
2010-06-04 12:09:05 -0400

    setup.py now works on my intel Mac
    2 problems fixed:
    
    1. location of numpy C++ header files (we now get the path from numpy)
    2. trying to build a universal binary when the dassl.o library was only compiled for i386

--- setup.py
+++ setup.py
@@ -10,9 +10,32 @@ if __name__ == '__main__':
 	import Cython.Compiler
 	Cython.Compiler.Options.annotate = True
 	
+	import numpy
+	
+	
+	# Stop wasting my time compiling PowerPC-compatible C extensions on my intel Mac
+	# especially important as we didn't build a PPC version of dassl.o
+	import distutils.sysconfig
+	config = distutils.sysconfig.get_config_vars()
+	for key,value in config.iteritems():
+		location = str(value).find('-arch ppc')
+		if location>=0:
+			print "removing '-arch ppc' from %s"%(key)
+			config[key] = value.replace('-arch ppc ','')
+	# Tip from Lisandro Dalcin:
+	# FYI, in Py>=2.6 the ARCHFLAGS environ var is honored by distutils, so
+	# you should be able to
+	#  export ARCHFLAGS='-arch i386' # or '-arch x86_64'
+	# and get your job done.
+	
+	
 	# The Cython modules to setup
 	ext_modules = [
-		Extension('dassl', ['dassl.pyx'], library_dirs=['dassl'], libraries=['gfortran', 'ddassl']),
+		Extension('dassl', ['dassl.pyx'], 
+			library_dirs=['dassl'], 
+			libraries=['gfortran', 'ddassl'],
+			include_dirs = [ numpy.get_include() ] # location of numpy headers
+			),
 	]
 
 	setup(cmdclass = {'build_ext': build_ext},
